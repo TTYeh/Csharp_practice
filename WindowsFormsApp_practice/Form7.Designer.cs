f88c626b2186152dc2952e463ca8353ad9e41732
TTYeh <35258945+TTYeh@users.noreply.github.com>
2022-12-14 10:12:22 +0800

    add ArrayList practice

---
+++ WindowsFormsApp_practice/Form7.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace WindowsFormsApp_practice
+{
+    partial class Form7
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBox_name = new System.Windows.Forms.TextBox();
+            this.txtBoxAge = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labShowEmp = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btn_remove = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(20, 170);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(203, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "35_ArrList(集合)_add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(7, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "姓名: ";
+            // 
+            // txtBox_name
+            // 
+            this.txtBox_name.Location = new System.Drawing.Point(78, 48);
+            this.txtBox_name.Name = "txtBox_name";
+            this.txtBox_name.Size = new System.Drawing.Size(100, 25);
+            this.txtBox_name.TabIndex = 2;
+            // 
+            // txtBoxAge
+            // 
+            this.txtBoxAge.Location = new System.Drawing.Point(78, 85);
+            this.txtBoxAge.Name = "txtBoxAge";
+            this.txtBoxAge.Size = new System.Drawing.Size(100, 25);
+            this.txtBoxAge.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(7, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "年齡: ";
+            // 
+            // labShowEmp
+            // 
+            this.labShowEmp.AutoSize = true;
+            this.labShowEmp.BackColor = System.Drawing.Color.Chartreuse;
+            this.labShowEmp.Font = new System.Drawing.Font("標楷體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labShowEmp.ForeColor = System.Drawing.Color.Red;
+            this.labShowEmp.Location = new System.Drawing.Point(343, 25);
+            this.labShowEmp.Name = "labShowEmp";
+            this.labShowEmp.Size = new System.Drawing.Size(99, 23);
+            this.labShowEmp.TabIndex = 5;
+            this.labShowEmp.Text = "員工:  ";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtBoxAge);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtBox_name);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(20, 15);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(203, 131);
+            this.panel1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(20, 228);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(203, 52);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "35_ArrList(集合)_insert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btn_remove
+            // 
+            this.btn_remove.Location = new System.Drawing.Point(20, 286);
+            this.btn_remove.Name = "btn_remove";
+            this.btn_remove.Size = new System.Drawing.Size(203, 52);
+            this.btn_remove.TabIndex = 8;
+            this.btn_remove.Text = "35_ArrList(集合)_remove";
+            this.btn_remove.UseVisualStyleBackColor = true;
+            this.btn_remove.Click += new System.EventHandler(this.btn_remove_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(20, 344);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(203, 52);
+            this.btn_clear.TabIndex = 9;
+            this.btn_clear.Text = "35_ArrList(集合)_clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // Form7
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_remove);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.labShowEmp);
+            this.Controls.Add(this.button1);
+            this.Name = "Form7";
+            this.Text = "Form7";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBox_name;
+        private System.Windows.Forms.TextBox txtBoxAge;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labShowEmp;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btn_remove;
+        private System.Windows.Forms.Button btn_clear;
+    }
+}
